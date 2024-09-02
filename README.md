45109cafe26f86bb73a1a0d7803ce7f10ec7aea2
Ken Guo <ken.guo1105@gmail.com>
2024-09-02 12:12:44 -0700

    add pwsh script

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Powershell script to set VM configuration for real-time settings and add paramet
 The Powershell script reserves proper CPU (example shows 2 physical cores) and all memory assigned to the VM, and configures high-latency sensitivity. Additionally, it also adds the extra configuration parameters in the .vmx file for the VM to be compatible with Industrial vSwitch feature (currently in Beta).
 ![reserve_cpu_and_memory](https://github.com/user-attachments/assets/2861b904-57b7-4b8e-9c87-6e5f6b4a1a30)
 ![set_high_latency_sensitivity](https://github.com/user-attachments/assets/c57c5706-8f47-4173-b0bf-54580af227da)
+
+### Script Usage
+Run the script in Powershell with the virtual machine name as the only argument
+
+Example syntax:
+PS /Users/keng/Github/vc-vm-rt-vswitch> ./set-vm-rt-settings.ps1 <vm_name>
---
+++ set-vm-rt-settings.ps1
@@ -0,0 +1,70 @@
+# Import the VMware.PowerCLI module if it's not already loaded
+if (-not (Get-Module -Name VMware.PowerCLI -ErrorAction SilentlyContinue)) {
+    Import-Module VMware.PowerCLI
+}
+
+# Set your vCenter Server information
+$server = "vcenter.vmware.com"
+$username = ""
+$password = ""
+
+# Get the virtual machine name as the first command-line argument
+$vmName = $args[0]
+
+if (-not $vmName) {
+    Write-Host "Please provide the virtual machine name as the first argument."
+    Exit
+}
+
+$vmxSettings = @{
+    "virtualHW.version" = "21"
+    "ethernet1.realtime" = "TRUE"
+    "vmxnet3.rev.90" = "TRUE"
+#    "sched.mem.pin" = "TRUE"
+#    "sched.mem.prealloc" = "TRUE"
+#    "sched.mem.pinnedMainMem" = "TRUE"
+#    "monitor.forceEnableMPTI" = "TRUE"
+#    "sched.swap.vmxSwapEnabled" = “FALSE”
+#    "numa.nodeAffinity" = "1"
+    # Add more settings as needed
+}
+
+# Connect to the vCenter Server
+Connect-VIServer -Server $server -User $username -Password $password
+
+# Get the virtual machine by name
+$vm = Get-VM -Name $vmName
+
+if ($vm) {
+    # Check the current power state of the VM
+    $vmPowerState = $vm.PowerState
+
+    # Reserve CPU resources with a value of 5188 MHz and set memory reservation.
+    # Configure latency sensitivity to "high"
+    $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
+    $spec.cpuAllocation = New-Object VMware.Vim.ResourceAllocationInfo
+    $spec.cpuAllocation.Reservation = 5188  # Set your desired reservation in MHz
+    $spec.memoryReservationLockedToMax = $true
+    $spec.latencySensitivity = New-Object VMware.Vim.LatencySensitivity
+    $spec.LatencySensitivity.Level = [VMware.Vim.LatencySensitivitySensitivityLevel]::high
+    $vm.ExtensionData.ReconfigVM($spec)
+
+     # Loop through the settings and update them in the virtual machine's .vmx file
+    foreach ($key in $vmxSettings.Keys) {
+        $value = $vmxSettings[$key]
+
+        # Check if the setting exists, and if so, update it; otherwise, create a new setting
+        $existingSetting = Get-AdvancedSetting -Entity $vm -Name $key -ErrorAction SilentlyContinue
+        if ($existingSetting) {
+            Set-AdvancedSetting -AdvancedSetting $existingSetting -Value $value -Confirm:$false
+        } else {
+            New-AdvancedSetting -Entity $vm -Name $key -Value $value -Confirm:$false
+        }
+    }
+}
+else {
+    Write-Host "Virtual machine $vmName not found."
+}
+
+# Disconnect from the vCenter Server
+Disconnect-VIServer -Server $server -Confirm:$false
